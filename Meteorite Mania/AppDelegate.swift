b97efc42fa64c9be54218166dadebf6255bda737
Jonathan Wiley <wiley@lunarlincoln.com>
2018-04-21 23:01:53 -0500

    Moved the import off of the main thread.

--- Meteorite Mania/AppDelegate.swift
+++ Meteorite Mania/AppDelegate.swift
@@ -15,8 +15,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        MeteoritesCoreDataStore.importNASAMeteoritesCSV(csvFileName: "Meteorite_Landings")
+        
+        DispatchQueue(label: "ImportQueue").async {
+            MeteoritesCoreDataStore.importNASAMeteoritesCSV(csvFileName: "Meteorite_Landings")
+        }
+        
         return true
     }
 
